3148ef395c9867b7aa1ea43a2f989d3769a2214d
Om Patil <ompatil4529@gmail.com>
2026-01-08 22:18:42 +0530

    Add paths configuration

---
+++ config/paths_config.py
@@ -0,0 +1,10 @@
+import os
+
+RAW_DIR = "artifacts/raw"
+RAW_FILE_PATH = os.path.join(RAW_DIR, "Hotel_Reservations.csv")
+
+TRAIN_FILE_PATH = os.path.join(RAW_DIR, "train.csv")
+TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
+
+CONFIG_PATH = "config/config.yaml"
+
